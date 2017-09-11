3913003ca4e72c741ea0bdbefe5e29ecf41e5154
raghavenderlomte <raghavenderlomte@gmail.com>
2017-09-11 16:00:05 +0530

    Update server.c

--- test/server.c
+++ test/server.c
@@ -23,17 +23,17 @@ int main()
   
 	 memset(client_socket,0,sizeof(client_socket));
 	  
-	 if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) <= 0)       //creating the socket fd for tcp
+	 if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) <= 0)       /*creating the socket fd for tcp */
 	 {  
 	        perror("TCP socket failed");  
 	        return -1;memset(pckt,'\0',sizeof(struct packet));
 	 }  
-	 if( (udp_fd = socket(AF_INET , SOCK_DGRAM, 0)) <= 0)  			//creating the socket fd for udp
+	 if( (udp_fd = socket(AF_INET , SOCK_DGRAM, 0)) <= 0)  			/*creating the socket fd for udp*/
 	 {  
 	        perror("UDP socket failed");  
 	        return -1;
 	 }  
-	 if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,sizeof(opt)) < 0 )  //force fully assigning the address to 														socket fd
+	 if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,sizeof(opt)) < 0 )  /*force fully assigning the address to socket fd */
 	 {  
 	        perror("tcp setsockopt");  
 	    	return -1;
@@ -45,12 +45,12 @@ int main()
     	 }  
 	    
 
-	    address.sin_family = AF_INET;						//filling the server details  
+	    address.sin_family = AF_INET;						/*filling the server details*/  
 	    address.sin_addr.s_addr = inet_addr(ADDRESS); 
 	    address.sin_port = htons(PORT);  
 
 
-	    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)   //binding the server address and port num with 												socket_fd
+	    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)   /*binding the server address and port num with socket_fd */
 	    {  
 	        perror("tcp bind failed");  
 		return -1;
@@ -63,7 +63,7 @@ int main()
 
         
 
-	    if (listen(master_socket, 3) < 0)  				//mentioning the pending que list for tcp
+	    if (listen(master_socket, 3) < 0)  				/*mentioning the pending que list for tcp*/
 	    {  
 
 	        perror("listen");  
@@ -94,12 +94,12 @@ int main()
 				    max_sd = sd;  
 		            }
 			}
-		        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);   //when ever there is an activity at fd with 													select it ets identified
+		        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);   /*when ever there is an activity at fd with select it ets identified*/
         		if ((activity < 0) && (errno!=EINTR))  
 		        {  
 		            printf("select error");  
 		        }  
-      		        if ((FD_ISSET(master_socket, &readfds)))			//checking whether the activity is at tcp_socket
+      		        if ((FD_ISSET(master_socket, &readfds)))			/*checking whether the activity is at tcp_socket*/
         		{  
 		            new_socket =accept(master_socket,(struct sockaddr *)&client_addr, (socklen_t*)&addrlen);
 	    		    if(new_socket == -1)  
@@ -119,12 +119,12 @@ int main()
         			        }  
 			    }  
 		        }  
-			else if((FD_ISSET(udp_fd,&readfds)))  			//checking whether the activity is at udp_socket
+			else if((FD_ISSET(udp_fd,&readfds)))  			/*checking whether the activity is at udp_socket*/
 			{
 				recvfrom(udp_fd,buffer,sizeof(buffer),0,(struct sockaddr *)&client_addr,&addrlen);
 				time(&rawtime);
 				timeinfo=localtime(&rawtime);
-				strcpy(hash_timedata,(char *)asctime(timeinfo));//time stamp is stored in vrbl
+				strcpy(hash_timedata,(char *)asctime(timeinfo));/*time stamp is stored in vrbl*/
 				getpeername(sd , (struct sockaddr*)&client_addr,(socklen_t*)&addrlen);
 				pckt=(struct packet*)malloc(sizeof(struct packet)); 
 				memset(pckt,'\0',sizeof(struct packet));
@@ -145,7 +145,7 @@ int main()
 		         }
         		for (i = 0; i < max_clients; i++)  
 		        {  
-		            sd = client_socket[i];  			//checking whether the activity is at any of the exisintg fd's.
+		            sd = client_socket[i];  			/*checking whether the activity is at any of the exisintg fd's.*/
               	            if (FD_ISSET( sd , &readfds))  
         		    {  
                 	            if ((valread = read( sd , buf,sizeof(buf))) == 0)  
@@ -162,20 +162,20 @@ int main()
                     
 					 
 						printf("read\n");
-	                    			read(sd,buf,sizeof(buf));  //waiting for client to read some data
+	                    			read(sd,buf,sizeof(buf));  /*waiting for client to read some data*/
 						//printf("%s",buf);
 						pt=(struct packet *)buf;
 						pt->protocol=6;
 						pt->src=ntohl(pt->src);
 						pt->dest=ntohl(pt->dest);
 						printf("%s\n",pt->time);
-						if(strcmp(pt->time,"look")==0)	//for cehcking all the time stamps of a particular entry
+						if(strcmp(pt->time,"look")==0)	/*for cehcking all the time stamps of a particular entry*/
 						{
 							hash_lookup((void *)pt);
 							memset(pt,0,sizeof(struct packet));		
 								break;
 						}
-						if(strcmp(pt->time,"delete")==0)	//for deletion a particular entry
+						if(strcmp(pt->time,"delete")==0)	/*for deletion a particular entry*/
 						{
 							hash_del((void *)pt);
 							memset(pt,0,sizeof(struct packet));		
